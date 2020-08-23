bb9913b9066fabea054a1166329a73a2156000d1
Mark Mizen <mark.c.mizen@gmail.com>
2020-08-23 21:07:32 +0100

    deploy

---
+++ precache-manifest.562dd330dbed7e48f4bda7e12d8c6d69.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0b23eac5fdba7734dcac",
+    "url": "/air-quality/css/app.2ac412a9.css"
+  },
+  {
+    "revision": "c4bb6fdb40d37dfefb2242507feedb8e",
+    "url": "/air-quality/img/arrow.c4bb6fdb.svg"
+  },
+  {
+    "revision": "d20bec82f24febc756e3b7bb46216e81",
+    "url": "/air-quality/img/target-focus.d20bec82.svg"
+  },
+  {
+    "revision": "fe292d782b6b9f173650488ea9845372",
+    "url": "/air-quality/index.html"
+  },
+  {
+    "revision": "1497c6fb0afab87d0ea4",
+    "url": "/air-quality/js/about.1409afb0.js"
+  },
+  {
+    "revision": "0b23eac5fdba7734dcac",
+    "url": "/air-quality/js/app.3770c091.js"
+  },
+  {
+    "revision": "7a29afc61e794f00ca6a",
+    "url": "/air-quality/js/chunk-vendors.0d7b29b6.js"
+  },
+  {
+    "revision": "d4415705c03f1131dc1c5613cea85497",
+    "url": "/air-quality/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/air-quality/robots.txt"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/air-quality/precache-manifest.562dd330dbed7e48f4bda7e12d8c6d69.js"
+);
+
+workbox.core.setCacheNameDetails({prefix: "air-quality"});
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
